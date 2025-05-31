cd9b46dc4573b7e813d03b12ad07bd0a48794d35
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2025-05-31 20:12:15 +0300

    feat: create SoftwareEngineerRepository for data access layer

---
+++ src/main/java/com/kipruto/SoftwareEngineerRepository.java
@@ -0,0 +1,7 @@
+package com.kipruto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareEngineerRepository
+        extends JpaRepository<SoftwareEngineer, Integer> {
+}
