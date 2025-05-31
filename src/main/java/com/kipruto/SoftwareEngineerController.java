d6fd2b009ac205363deb71a53fc929c2f25a3ec2
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2025-05-31 20:14:35 +0300

    refactor: implement proper layered architecture with service injection and add POST/GET by ID endpoints

--- src/main/java/com/kipruto/SoftwareEngineerController.java
+++ src/main/java/com/kipruto/SoftwareEngineerController.java
@@ -1,27 +1,33 @@
 package com.kipruto;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping ("api/v1/software-engineers")
 public class SoftwareEngineerController {
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
     @GetMapping
     public List<SoftwareEngineer> getSoftwareEngineers() {
-        return List.of(
-                new SoftwareEngineer(
-                        1,
-                        "Mobjot",
-                        List.of("Java, Node, React")
-                ),
-                new SoftwareEngineer(
-                        2,
-                        "Otongo",
-                        List.of("C, C#, Assembly")
-                )
-        );
+        return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getSoftwareEngineersById(
+            @PathVariable Integer id
+    ) {
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(
+            @RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
     }
 }
